260152bbb2f882a4dea7923e7891768e2454bda3
Alex <snoopcom@bk.ru>
2020-06-01 11:21:23 +0300

    test img

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Мой проект</h1>
+      <h2>test</h2>
+      <img src={logo} alt="image" />
     </div>
   );
 }
